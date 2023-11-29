c77f2bb96c887fc7cd564ee92972f6aaa42bf43a
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2023-11-29 18:30:07 +0530

    Update response message in API handler

--- src/pages/api/get-answer-from-files.ts
+++ src/pages/api/get-answer-from-files.ts
@@ -33,7 +33,7 @@ const handler = async (req: Request): Promise<Response> => {
 
     const prompt = HUMAN_PROMPT +
       `Given a question, try to answer it using the content of the file extracts below, and if you cannot answer, or find a relevant file, just output \"I couldn't find the answer to that question in your files.\".\n\n` +
-      `If the answer is not contained in the files or if there are no file extracts, respond with \"I couldn't find the answer to that question in your files.\" If the question is not actually a question, respond with \"That's not a valid question.\" You just and just have to answer the question and not do anything until asked!\n\n` +
+      `If the answer is not contained in the files or if there are no file extracts, respond with \"I couldn't find the answer to that question in your files.\" If the question is not actually a question, respond with \"That's not a valid question.\" YOU DO NOT HAVE TO SAY ANYTHING ELSE OR QUESTION ON YOUR OWN AT ALL COSTS!\n\n` +
       `In the cases where you can find the answer, first give the answer. Then explain how you found the answer from the source or sources, and use the exact filenames of the source files you mention. Do not make up the names of any other files other than those mentioned in the files context. Give the answer in markdown format.` +
       `Use the following format:\n\nQuestion: <question>\n\nFiles:\n<###\n\"filename 1\"\nfile text>\n<###\n\"filename 2\"\nfile text>...\n\nAnswer: <answer or "I couldn't find the answer to that question in your files" or "That's not a valid question.">\n\n` +
       `Question: ${question}\n\n` +
